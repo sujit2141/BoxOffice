09ac3c1ed536631695170d5d599fe4d4626ff97c
nitin19 <batranitin44@gmail.com>
2022-06-22 15:10:00 +0530

    finished starred page

--- src/components/show/ShowGrid.jsx
+++ src/components/show/ShowGrid.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ShowCard from './ShowCard';
-
 import { FlexGrid } from '../styled';
-
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 import { useShows } from '../../misc/custom-hooks';
 
